7f6225b1f1ca479e20326ca450492d2c7a1c4daa
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-07-21 15:07:46 +0300

    feat: add foundation for future addtional tracks fetching

--- src/components/TrackCarousel.tsx
+++ src/components/TrackCarousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation } from "swiper/modules";
 import TrackCard from "./TrackCard";
@@ -8,21 +9,38 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 
-export default function TrackCarousel({tracks}: {tracks: any[]}) {
-    return (
-        <Swiper
-        modules={[Navigation, FreeMode]}
-        spaceBetween={20}
-        slidesPerView="auto"
-        freeMode={true}
-        navigation
-        className="w-full pb-4 !px-1"
-      >
-        {tracks.map((track) => (
-          <SwiperSlide key={track.trackId} style={{width: "auto"}}>
-            <TrackCard track={track} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    )
+export default function TrackCarousel({tracks: initialTracks}: {tracks: any[]}) {
+  const [tracks, setTracks] = useState(initialTracks);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+  const handleReachEnd = async () => {
+    if (isLoadingMore) return; 
+    setIsLoadingMore(true);
+
+    try {
+    
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setIsLoadingMore(false);
+    }
+  };
+
+  return (
+    <Swiper
+      modules={[Navigation, FreeMode]}
+      spaceBetween={20}
+      slidesPerView="auto"
+      freeMode={true}
+      navigation
+      onReachEnd={handleReachEnd}
+      className="w-full pb-4 !px-1"
+    >
+      {tracks.map((track) => (
+        <SwiperSlide key={track.trackId} style={{ width: "auto" }}>
+          <TrackCard track={track} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
