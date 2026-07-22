bb04c91dd517bc8e91ab625a42be9be82acf3907
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-07-22 17:16:20 +0300

    feat: add auth context

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { PlaylistsProvider } from "@/context/PlaylistsContext";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function Providers({ children }) {
-  return <PlaylistsProvider>{children}</PlaylistsProvider>;
+  return (
+    <AuthProvider>
+      <PlaylistsProvider>
+          {children}
+        </PlaylistsProvider>;
+    </AuthProvider>
+  )
 }
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -17,3 +17,48 @@ type AuthContextType = {
 };
 
 const AuthContext = createContext(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+    const [user, setUser] = useState<User | null>(null);;
+    const [token, setToken] = useState<string | null>(null);;
+    const [isHydrated, setIsHydrated] = useState(false);
+    const router = useRouter();
+  
+    useEffect(() => {
+      const storedToken = localStorage.getItem("token");
+      const storedUser = localStorage.getItem("user");
+  
+      if (storedToken && storedUser) {
+        setToken(storedToken);
+        setUser(JSON.parse(storedUser));
+      }
+      setIsHydrated(true);
+    }, []);
+  
+    const login = (newToken: string, newUser: User) => {
+      localStorage.setItem("token", newToken);
+      localStorage.setItem("user", JSON.stringify(newUser));
+      setToken(newToken);
+      setUser(newUser);
+    };
+  
+    const logout = () => {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      setToken(null);
+      setUser(null);
+      router.push("/login");
+    };
+  
+    return (
+        {children}
+    );
+  }
+  
+  export function useAuth() {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+      throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+  }
